727f3aea64131c0190eaea415d65b1ab4875eef1
dhandeprajakta18 <dhandeprajakta18@gmail.com>
2026-01-13 16:45:26 +0530

    13/1/26

--- data/products.js
+++ data/products.js
@@ -767,7 +767,7 @@
   storage: "Store at -20°C. Avoid repeated freeze–thaw cycles.",
   researchStatus: "For laboratory research use only. Not for human or veterinary use.",
 
-  image: "/product/peptides/aicar.png"
+  image: "/product/peptides/aicar_50mg.png"
 },
 {
   id: "ara-290-16mg",
